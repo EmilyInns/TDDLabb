5b4177eeb19664e7f758f6fa7c481ab0a528a7bb
Emily Inns <nyssa.inns@gmail.com>
2020-10-22 21:27:25 +0200

    Initial commit, made main class and created some possible tests for future implementation

---
+++ src/test/java/mainTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class mainTest {
+
+    @Test
+    void DoesBoardSetUpCorrectly(){
+
+    }
+
+    @Test
+    void DoesBoardUpdateCorrectly(){
+
+    }
+
+    @Test
+    void DoesBoardReadInCorrectly(){
+
+    }
+
+    @Test
+    void DoCellsDieOfUnderpop(){
+
+    }
+
+    @Test
+    void DoCellsDieOfOvercrowding(){
+
+    }
+
+    @Test
+    void DoCellsLiveInGoodConditions(){
+
+    }
+
+    @Test
+    void DoCellsReviveWithThreeLivingNeighbours(){
+
+    }
+
+
+
+}
