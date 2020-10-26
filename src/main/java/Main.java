468a9e6a894185f9abb8f73bd6cb37e7c832ea47
EmilyInns <nyssa.inns@gmail.com>
2020-10-26 09:00:21 +0100

    Working towards tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,14 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+//TODO: Refactor code with object to remove constant listIndex and subIndex usage
+
 
 public class Main {
 
     private int numberOfLines = 0;
+    private int numberOfArrays = 0;
+    private int lineLength = 0;
     public List<List<Character>> board = new ArrayList<List<Character>>();
 
     public void readBoard() {
@@ -25,14 +29,71 @@ public class Main {
                         mylist.add(curLine.charAt(i));
                     }
                     board.add(mylist);
+                    lineLength = curLine.length();
                 }
                 System.out.println(curLine);
             }
+            numberOfArrays = numberOfLines-1;
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    public int numberOfLivingNeighbours(int listIndex, int subIndex){
+        int numberOfLivingNeighbours = 0;
+        if (isCellAlive(listIndex-1, subIndex-1)) numberOfLivingNeighbours++;
+        if (isCellAlive(listIndex-1, subIndex)) numberOfLivingNeighbours++;
+        if (isCellAlive(listIndex-1, subIndex+1)) numberOfLivingNeighbours++;
+        if (isCellAlive(listIndex, subIndex-1)) numberOfLivingNeighbours++;
+        if (isCellAlive(listIndex, subIndex+1)) numberOfLivingNeighbours++;
+        if (isCellAlive(listIndex+1, subIndex-1)) numberOfLivingNeighbours++;
+        if (isCellAlive(listIndex+1, subIndex)) numberOfLivingNeighbours++;
+        if (isCellAlive(listIndex+1, subIndex+1)) numberOfLivingNeighbours++;
+
+        return numberOfLivingNeighbours;
     }
+
+    public boolean isCellAlive(int listIndex, int subIndex){
+        if (listIndex<0||subIndex<0||listIndex>=numberOfArrays||subIndex>=lineLength){
+            return false;
+        }
+        else return board.get(listIndex).get(subIndex).equals('*');
+
+    }
+
+    public void checkIfCellStateNeedsUpdating(int listIndex, int subIndex){
+        boolean isAlive = board.get(listIndex).get(subIndex).equals('*');
+        boolean isCellStateChanged = false;
+        if (isAlive){
+            doesCellDieFromOverOrUnderPop(listIndex, subIndex);
+        }
+        else{
+            doesCellRevive(listIndex,subIndex);
+        }
+    }
+
+    private void doesCellRevive(int listIndex, int subIndex) {
+        if (numberOfLivingNeighbours(listIndex,subIndex)<2){
+            changeCellState(listIndex, subIndex);
+        }
+    }
+
+    private void doesCellDieFromOverOrUnderPop(int listIndex, int subIndex) {
+        if ((numberOfLivingNeighbours(listIndex,subIndex)<2)||(numberOfLivingNeighbours(listIndex,subIndex)>3)){
+            changeCellState(listIndex,subIndex);
+        }
+    }
+
+    private void changeCellState(int listIndex, int subIndex) {
+        if (board.get(listIndex).get(subIndex).equals('*')){
+            board.get(listIndex).set(subIndex,'.');
+        }
+        else if (board.get(listIndex).get(subIndex).equals('.')){
+            board.get(listIndex).set(subIndex,'*');
+        }
+    }
+
+
+}
 
--- src/test/java/mainTest.java
+++ src/test/java/mainTest.java
@@ -12,8 +12,12 @@ class mainTest {
     @Test
     void DoesBoardReadInCorrectly(){
         Main main = new Main();
-    main.readBoard();
-        assertEquals('.', main.board.get(1).get(1));
+        main.readBoard();
+        assertEquals('*', main.board.get(0).get(2));
+        assertEquals('.', main.board.get(1).get(2));
+        assertEquals('*', main.board.get(1).get(6));
+        assertEquals('*', main.board.get(2).get(3));
+        assertEquals('*', main.board.get(3).get(3));
 
     }
 
@@ -24,7 +28,9 @@ class mainTest {
 
     @Test
     void DoCellsKnowTheirNeighboursStates(){
-
+        Main main = new Main();
+        main.readBoard();
+        assertEquals(2,main.numberOfLivingNeighbours(2,2));
     }
 
     @Test
